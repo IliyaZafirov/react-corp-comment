ee1c536c9212212cc1d4cdfb6787d37bb6096c79
Shaker <133087050+IliyaZafirov@users.noreply.github.com>
2024-09-11 21:28:48 +0300

    fetch, set state and loading spinner while fetching data

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,24 +1,36 @@
+import { useEffect, useState } from "react";
 import FeedbackItem from "./FeedbackItem";
-
-const feedbackItems = [
-  {
-    voteCount: 593,
-    badgeLetter: "B",
-    companyName: "ByteGrad",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis velit labore repellendus eos perspiciatis eaque.",
-    daysAgo: 4,
-  },
-];
+import Spinner from "./Spinner";
 
 export default function FeedbackList() {
+  const [feedbackItems, setFeedbackItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setFeedbackItems(data.feedbacks);
+        setIsLoading(false);
+      });
+  }, []);
+
   return (
     <ol className="feedback-list">
+      {isLoading ? <Spinner /> : null}
+      
       {/* {feedbackItems.map((feedbackItem) => {
         return <FeedbackItem feedbackItem={feedbackItem} />;
       })} */}
 
-{feedbackItems.map(feedbackItem => 
-(<FeedbackItem feedbackItem={feedbackItem} />))}
+      {feedbackItems.map((feedbackItem) => (
+        <FeedbackItem key={feedbackItem.id} feedbackItem={feedbackItem} />
+      ))}
     </ol>
   );
 }
