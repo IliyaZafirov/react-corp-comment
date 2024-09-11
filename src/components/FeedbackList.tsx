4689dfafe4d5ea4c2dcfaa3bf2624e2bde2865b9
Shaker <133087050+IliyaZafirov@users.noreply.github.com>
2024-09-11 22:05:06 +0300

    added error message when fetching went wrong

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import FeedbackItem from "./FeedbackItem";
 import Spinner from "./Spinner";
+import ErrorMessage from "./ErrorMessage";
 
 export default function FeedbackList() {
   const [feedbackItems, setFeedbackItems] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
   useEffect(() => {
     setIsLoading(true);
@@ -12,10 +14,17 @@ export default function FeedbackList() {
       "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
     )
       .then((response) => {
+        if (!response.ok) {
+          throw new Error("Something went wr432432ong.");
+        }
         return response.json();
       })
       .then((data) => {
         setFeedbackItems(data.feedbacks);
+        setIsLoading(false);
+      })
+      .catch(() => {
+        setErrorMessage(`Something went wrong.`);
         setIsLoading(false);
       });
   }, []);
@@ -23,7 +32,9 @@ export default function FeedbackList() {
   return (
     <ol className="feedback-list">
       {isLoading ? <Spinner /> : null}
-      
+
+      {errorMessage ? <ErrorMessage message={errorMessage} /> : null}
+
       {/* {feedbackItems.map((feedbackItem) => {
         return <FeedbackItem feedbackItem={feedbackItem} />;
       })} */}
