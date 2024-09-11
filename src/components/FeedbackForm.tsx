c204dad68350b8dc03ef721769955ba37bb04761
Shaker <133087050+IliyaZafirov@users.noreply.github.com>
2024-09-11 07:21:40 +0300

    project setup

---
+++ src/components/FeedbackForm.tsx
@@ -0,0 +1,23 @@
+export default function FeedbackForm() {
+  return (
+    <form className="form">
+      <textarea
+        id="feedback-textarea"
+        placeholder="123"
+        spellCheck={false}
+        maxLength={150}
+      />
+
+      <label htmlFor="feedback-textarea">
+        Enter your feedback here, remeber to #hashtag the company
+      </label>
+
+      <div>
+        <p className="u-italic">150</p>
+        <button>
+          <span>Submit</span>
+        </button>
+      </div>
+    </form>
+  );
+}
