5051fbed0e573869b16214026628abe85d332ebe
Shaker <133087050+IliyaZafirov@users.noreply.github.com>
2024-09-12 16:29:55 +0300

    disable upvote button after click

--- src/components/feedback/FeedbackItem.tsx
+++ src/components/feedback/FeedbackItem.tsx
@@ -10,6 +10,7 @@ export default function FeedbackItem({ feedbackItem }: FeedbackItemProps) {
 
   const handleUpvote = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     setUpvoteCount((prev) => ++prev);
+    e.currentTarget.disabled = true;
     e.stopPropagation();
   };
 
