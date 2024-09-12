34aeb7a437ca8be7c8c5916aed340f07262cf0f8
Shaker <133087050+IliyaZafirov@users.noreply.github.com>
2024-09-12 08:50:43 +0300

    add item to list functionality

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,86 @@
+import { useEffect, useState } from "react";
 import Container from "./Container";
 import Footer from "./Footer";
 import HashtagList from "./HashtagList";
+import { TFeedbackItem } from "./lib/types";
 
 function App() {
+  const [feedbackItems, setFeedbackItems] = useState<TFeedbackItem[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleAddToList = (text: string) => {
+    const companyName = text
+      .split(" ")
+      .find((word) => word.includes("#"))!
+      .substring(1);
+
+    const newItem: TFeedbackItem = {
+      id: new Date().getTime(),
+      text,
+      voteCount: 0,
+      daysAgo: 0,
+      companyName: companyName,
+      badgeLetter: companyName.substring(0, 1).toUpperCase(),
+    };
+
+    setFeedbackItems([...feedbackItems, newItem]);
+  };
+
+  useEffect(() => {
+    const fetchFeedbackItems = async () => {
+      setIsLoading(true);
+
+      try {
+        const response = await fetch(
+          "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
+        );
+
+        if (!response.ok) {
+          throw new Error();
+        }
+
+        const data = await response.json();
+        setFeedbackItems(data.feedbacks);
+      } catch (error) {
+        setErrorMessage(`Something went wrong.`);
+      }
+
+      setIsLoading(false);
+    };
+
+    fetchFeedbackItems();
+
+    // setIsLoading(true);
+    // fetch(
+    //   "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
+    // )
+    //   .then((response) => {
+    //     if (!response.ok) {
+    //       throw new Error();
+    //     }
+    //     return response.json();
+    //   })
+    //   .then((data) => {
+    //     setFeedbackItems(data.feedbacks);
+    //     setIsLoading(false);
+    //   })
+    //   .catch(() => {
+    //     setErrorMessage(`Something went wrong.`);
+    //     setIsLoading(false);
+    //   });
+  }, []);
+
   return (
     <div className="app">
       <Footer />
 
-      <Container />
+      <Container
+        isLoading={isLoading}
+        errorMessage={errorMessage}
+        feedbackItems={feedbackItems}
+        handleAddToList={handleAddToList}
+      />
 
       <HashtagList />
     </div>
--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,57 +1,19 @@
-import { useEffect, useState } from "react";
 import FeedbackItem from "./FeedbackItem";
 import Spinner from "./Spinner";
 import ErrorMessage from "./ErrorMessage";
+import { TFeedbackItem } from "./lib/types";
 
-export default function FeedbackList() {
-  const [feedbackItems, setFeedbackItems] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [errorMessage, setErrorMessage] = useState("");
+type FeedbackListProps = {
+  isLoading: boolean;
+  feedbackItems: TFeedbackItem[];
+  errorMessage: string;
+};
 
-  useEffect(() => {
-    const fetchFeedbackItems = async () => {
-      setIsLoading(true);
-
-      try {
-        const response = await fetch(
-          "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
-        );
-
-        if (!response.ok) {
-          throw new Error();
-        }
-
-        const data = await response.json();
-        setFeedbackItems(data.feedbacks);
-      } catch (error) {
-        setErrorMessage(`Something went wrong.`);
-      }
-
-      setIsLoading(false);
-    };
-
-    fetchFeedbackItems();
-
-    // setIsLoading(true);
-    // fetch(
-    //   "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
-    // )
-    //   .then((response) => {
-    //     if (!response.ok) {
-    //       throw new Error();
-    //     }
-    //     return response.json();
-    //   })
-    //   .then((data) => {
-    //     setFeedbackItems(data.feedbacks);
-    //     setIsLoading(false);
-    //   })
-    //   .catch(() => {
-    //     setErrorMessage(`Something went wrong.`);
-    //     setIsLoading(false);
-    //   });
-  }, []);
-
+export default function FeedbackList({
+  isLoading,
+  errorMessage,
+  feedbackItems,
+}: FeedbackListProps ) {
   return (
     <ol className="feedback-list">
       {isLoading ? <Spinner /> : null}
